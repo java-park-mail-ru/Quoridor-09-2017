bb0b045b84115f91d4e106b2ecd37a8536359bbe
Daniil <danil.ermoshin7@mail.ru>
2017-10-10 00:15:48 +0300

    Updated UserServiceTest, added settings for heroku

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -8,9 +8,11 @@ import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = Application.class)
 @RunWith(SpringRunner.class)
+@Transactional
 public class UserServiceTest {
 
     @Autowired
@@ -21,41 +23,47 @@ public class UserServiceTest {
 
     @Before
     public void setup() {
-        UserServiceTest.testId = userService.addUser("test", "12345", "test@mail.ru");
+        UserServiceTest.testId = userService.addUser("test12345", "12345", "test12345@mail.ru");
         UserServiceTest.testUser = userService.getUserById(UserServiceTest.testId);
     }
 
     @Test
     public void addUser() {
-        final long id = userService.addUser("daniil", "12345", "daniil@mail.ru");
+        final long id = userService.addUser("daniil12345", "12345", "daniil12345@mail.ru");
         assertTrue(id > 0);
     }
 
-//    @Test
-//    public void getUserByEmailOrLogin() {
-//        final User user1 = userService.getUserByEmailOrLogin("test");
-//        assertNotNull(user1);
-//        assertEquals(user1.getLogin(), UserServiceTest.testUser.getLogin());
-//
-//        final User user2 = userService.getUserByEmailOrLogin("test@mail.ru");
-//        assertNotNull(user2);
-//        assertEquals(user2.getLogin(), UserServiceTest.testUser.getLogin());
-//    }
+    @Test
+    public void getUserByEmailOrLogin() {
+        final User user1 = userService.getUserByEmailOrLogin("test12345");
+        assertNotNull(user1);
+        assertEquals(user1.getLogin(), UserServiceTest.testUser.getLogin());
+
+        final User user2 = userService.getUserByEmailOrLogin("test12345@mail.ru");
+        assertNotNull(user2);
+        assertEquals(user2.getLogin(), UserServiceTest.testUser.getLogin());
+
+        final User user3 = userService.getUserByEmailOrLogin("asdfg");
+        assertNull(user3);
+    }
 
     @Test
     public void getUserById() {
         final User user = userService.getUserById(UserServiceTest.testId);
-        assertEquals(user, UserServiceTest.testUser);
+        assertEquals(user.getId(), UserServiceTest.testUser.getId());
+        assertEquals(user.getLogin(), UserServiceTest.testUser.getLogin());
+        assertEquals(user.getEmail(), UserServiceTest.testUser.getEmail());
+        assertEquals(user.getPassword(), UserServiceTest.testUser.getPassword());
     }
 
     @Test
     public void emailExists() {
-        assertTrue(userService.emailExists("test@mail.ru"));
+        assertTrue(userService.emailExists("test12345@mail.ru"));
     }
 
     @Test
     public void loginExists() {
-        assertTrue(userService.loginExists("test"));
+        assertTrue(userService.loginExists("test12345"));
     }
 
     @Test
@@ -66,28 +74,31 @@ public class UserServiceTest {
 
     @Test
     public void changeEmail() {
-        final User user1 = userService.getUserById(UserServiceTest.testId);
-        userService.changeEmail(UserServiceTest.testId, "newTest@mail.ru");
-        final User user2 = userService.getUserById(UserServiceTest.testId);
-        assertEquals(user1.getEmail(), user2.getEmail());
-        assertEquals(user2.getEmail(), "newTest@mail.ru");
+        addUser();
+        final User user1 = userService.getUserByEmailOrLogin("daniil12345");
+        userService.changeEmail(user1.getId(), "newDaniil12345@mail.ru");
+        final User user2 = userService.getUserByEmailOrLogin("daniil12345");
+        assertNotEquals(user1.getEmail(), user2.getEmail());
+        assertEquals(user2.getEmail(), "newDaniil12345@mail.ru");
     }
 
     @Test
     public void changeLogin() {
-        final User user1 = userService.getUserById(UserServiceTest.testId);
-        userService.changeLogin(UserServiceTest.testId, "newTest");
-        final User user2 = userService.getUserById(UserServiceTest.testId);
-        assertEquals(user1.getLogin(), user2.getLogin());
-        assertEquals(user2.getLogin(), "newTest");
+        addUser();
+        final User user1 = userService.getUserByEmailOrLogin("daniil12345@mail.ru");
+        userService.changeLogin(user1.getId(), "newDaniil12345");
+        final User user2 = userService.getUserByEmailOrLogin("daniil12345@mail.ru");
+        assertNotEquals(user1.getLogin(), user2.getLogin());
+        assertEquals(user2.getLogin(), "newDaniil12345");
     }
 
     @Test
     public void changePassword() {
-        final User user1 = userService.getUserById(UserServiceTest.testId);
-        userService.changePassword(UserServiceTest.testId, "67890");
-        final User user2 = userService.getUserById(UserServiceTest.testId);
-        assertEquals(user1.getPassword(), user2.getPassword());
+        addUser();
+        final User user1 = userService.getUserByEmailOrLogin("daniil12345");
+        userService.changePassword(user1.getId(), "67890");
+        final User user2 = userService.getUserByEmailOrLogin("daniil12345");
+        assertNotEquals(user1.getPassword(), user2.getPassword());
         assertEquals(user2.getPassword(), "67890");
     }
 
