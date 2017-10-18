d8f851403056dc60beca90d8d77eed962b901e09
Daniil <danil.ermoshin7@mail.ru>
2017-10-18 21:07:47 +0300

    Updated SessionControllerTest

--- src/test/java/application/SessionControllerTest.java
+++ src/test/java/application/SessionControllerTest.java
@@ -1,6 +1,5 @@
 package application;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.http.MediaType;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,43 +22,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(print = MockMvcPrint.NONE)
-@Transactional
 public class SessionControllerTest {
 
-    @Autowired
+    @MockBean
     private UserService userService;
-
-    @Autowired
-    private ServiceForTests serviceForTests;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Before
     public void setup() {
-        serviceForTests.clearDB();
-        userService.addUser("test12345", "12345", "test12345@mail.ru");
+        final User user = new User(1L, "test", "12345", "test@mail.ru");
+        when(userService.emailExists("test@mail.ru")).thenReturn(true);
+        when(userService.loginExists("test")).thenReturn(true);
+        when(userService.getUserByEmailOrLogin("test")).thenReturn(user);
+        when(userService.getUserByEmailOrLogin("test@mail.ru")).thenReturn(user);
+        when(userService.getId(user)).thenReturn(1L);
+        when(userService.getUserById(1L)).thenReturn(user);
+        when(userService.checkPassword(1L, "12345")).thenReturn(true);
+        when(userService.addUser("test", "12345", "test@mail.ru")).thenReturn(1L);
     }
 
     @Test
     public void success_signin_with_login_or_email() throws Exception {
         mockMvc.perform(post("/signin")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"test12345\", \"password\":\"12345\"}"))
+                .content("{\"login\":\"test\", \"password\":\"12345\"}"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("login").value("test12345"))
-                .andExpect(jsonPath("email").value("test12345@mail.ru"));
+                .andExpect(jsonPath("login").value("test"))
+                .andExpect(jsonPath("email").value("test@mail.ru"));
 
-        mockMvc.perform(post("/signin")
+                mockMvc.perform(post("/signin")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"test12345@mail.ru\", \"password\":\"12345\"}"))
+                .content("{\"login\":\"test@mail.ru\", \"password\":\"12345\"}"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("login").value("test12345"))
-                .andExpect(jsonPath("email").value("test12345@mail.ru"));
+                .andExpect(jsonPath("login").value("test"))
+                .andExpect(jsonPath("email").value("test@mail.ru"));
     }
 
     @Test
     public void unsuccess_signin() throws Exception {
+        when(userService.emailExists("test1234567890@mail.ru")).thenReturn(false);
+
         mockMvc.perform(post("/signin")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"login\":\"test1234567890@mail.ru\", \"password\":\"12345\"}"))
@@ -67,23 +72,31 @@ public class SessionControllerTest {
 
         mockMvc.perform(post("/signin")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"test12345@mail.ru\", \"password\":\"12345678\"}"))
+                .content("{\"login\":\"test@mail.ru\", \"password\":\"12345678\"}"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("error").value("Wrong password"));
     }
 
     @Test
     public void success_signup() throws Exception {
+        when(userService.emailExists("daniil@mail.ru")).thenReturn(false);
+        when(userService.loginExists("daniil")).thenReturn(false);
+        when(userService.addUser("daniil", "12345", "daniil@mail.ru")).thenReturn(2L);
+        when(userService.getUserById(2L)).thenReturn(new User(2L, "daniil", "12345", "daniil@mail.ru"));
+
         mockMvc.perform(post("/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"daniil12345\", \"password\":\"12345\", \"email\":\"daniil12345@mail.ru\"}"))
+                .content("{\"login\":\"daniil\", \"password\":\"12345\", \"email\":\"daniil@mail.ru\"}"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("login").value("daniil12345"))
-                .andExpect(jsonPath("email").value("daniil12345@mail.ru"));
+                .andExpect(jsonPath("login").value("daniil"))
+                .andExpect(jsonPath("email").value("daniil@mail.ru"));
     }
 
     @Test
-    public void unsuccess_signun_login_or_email_allready_in_use() throws Exception {
+    public void unsuccess_signup_login_or_email_allready_in_use() throws Exception {
+        when(userService.emailExists("test12345@mail.ru")).thenReturn(true);
+        when(userService.loginExists("test12345")).thenReturn(true);
+
         mockMvc.perform(post("/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"login\":\"daniil12345\", \"password\":\"12345\", \"email\":\"test12345@mail.ru\"}"))
@@ -98,7 +111,7 @@ public class SessionControllerTest {
     }
 
     @Test
-    public void unsuccess_signun_with_bad_data() throws Exception {
+    public void unsuccess_signup_with_bad_data() throws Exception {
         mockMvc.perform(post("/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"login\":\"da\", \"password\":\"12\", \"email\":\"test_mail.ru\"}"))
@@ -112,15 +125,20 @@ public class SessionControllerTest {
     public void success_signout() throws Exception {
         mockMvc.perform(post("/signin")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"test12345\", \"password\":\"12345\"}"))
+                .content("{\"login\":\"test\", \"password\":\"12345\"}"))
                 .andDo(mvcResult -> mockMvc.perform(delete("/signout")
                         .sessionAttr("userId", mvcResult.getRequest().getSession().getAttribute("userId")))
                         .andExpect(status().isOk())
                         .andExpect(jsonPath("info").value("Successful logout")));
 
+        when(userService.emailExists("daniil@mail.ru")).thenReturn(false);
+        when(userService.loginExists("daniil")).thenReturn(false);
+        when(userService.addUser("daniil", "12345", "daniil@mail.ru")).thenReturn(2L);
+        when(userService.getUserById(2L)).thenReturn(new User(2L, "daniil", "12345", "daniil@mail.ru"));
+
         mockMvc.perform(post("/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"login\":\"daniil12345\", \"password\":\"12345\", \"email\":\"daniil12345@mail.ru\"}"))
+                .content("{\"login\":\"daniil\", \"password\":\"12345\", \"email\":\"daniil@mail.ru\"}"))
                 .andDo(mvcResult -> mockMvc.perform(delete("/signout")
                         .sessionAttr("userId", mvcResult.getRequest().getSession().getAttribute("userId")))
                         .andExpect(status().isOk())
@@ -134,8 +152,4 @@ public class SessionControllerTest {
                 .andExpect(jsonPath("error").value("Unauthorized"));
     }
 
-    @After
-    public void tearDown() {
-        serviceForTests.clearDB();
-    }
 }
